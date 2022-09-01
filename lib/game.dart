edc2480ab72ece0ff1ca49a478c4068c28f640f4
Arian <arian.foroutan.dev@gmail.com>
2022-09-01 13:52:32 +0430

    remove web file

--- lib/game.dart
+++ lib/game.dart
@@ -1,32 +1,3 @@
 class Game {
   static List<String> selectedchar = [];
-
-  List<String> alphabets = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'V',
-    'Z',
-  ];
 }
